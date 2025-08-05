2ce6e17e7ebc79585bbd2e6adc9ace35ba813da2
Abdallah-EL-Saied <abdallahalsabaa.pu.2021@gmail.com>
2025-08-05 20:07:29 +0300

    jQuery Day One

---
+++ week-005-(2-8-2025)/day-022/main.js
@@ -0,0 +1,83 @@
+$(document).ready(function () {
+
+    //================ Buttons Hover Effect ============================
+    $("#btns-box button").mouseenter(function () {
+        $(this).css({
+            "background-color": "red",
+            "color": "white",
+        });
+    });
+    $("#btns-box button").mouseleave(function () {
+        $(this).css({
+            "background-color": "#aaa",
+            "color": "unset"
+        });
+    });
+
+     // ========== Do: Apply all effects ==========
+    $("#do").click(function () {
+        $("#showHide").hide(500);
+        $("#fade").fadeOut(500);
+        $("#slide").slideUp(500);
+        $("#animation").animate({
+            width: "300px",
+            height: "70px",
+            opacity: 0.5
+        }, 500);
+    });
+
+    // ========== Undo: Reverse all effects ==========
+    $("#undo").click(function () {
+        $("#showHide").show(500);
+        $("#fade").fadeIn(500);
+        $("#slide").slideDown(500);
+        $("#animation").animate({
+            width: "100%",
+            height: "50px",
+            opacity: 1
+        }, 500);
+    });
+
+    // ========== Toggle: Toggle all effects ==========
+    $("#toggle").click(function () {
+        $("#showHide").toggle(500);
+        $("#fade").fadeToggle(500);
+        $("#slide").slideToggle(500);
+    });
+
+
+
+
+    //================ Show & Hide Effect ============================
+
+    $("#showHide").click(function () {
+        $(this).hide(500).show(500);
+    });
+
+    //================ Fade Effect ============================
+
+    $("#fade").dblclick(function () {
+        $(this).fadeOut(500).fadeIn(500);
+    });
+
+    //================ Slide Effect ============================
+
+    $("#slide").mouseenter(function () {
+        $(this).slideUp(500).slideDown(500);
+    });
+
+    //================ Animation Effect ============================
+
+    $("#animation").mouseenter(function () {
+        $(this).animate({
+            width: "300px",
+            height: "70px",
+            opacity: 0.5
+        }, 500).animate({
+            width: "100%",
+            height: "50px",
+            opacity: 1
+        }, 500);
+    });
+
+});
